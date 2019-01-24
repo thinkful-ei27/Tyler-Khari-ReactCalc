94f43dce2c0bec9581e0e5a72ab39881e9f2641b
Tyler Crabb <tydacrabb@gmail.com>
2019-01-24 13:52:12 -0800

    display works

--- src/App.js
+++ src/App.js
@@ -4,42 +4,44 @@ import Input from './input'
 import ClearButton from './clear';
 import EqualButton from './equalbutton';
 import OpButton from './opButton';
+import Display from './display';
 
 export default class App extends React.Component{
 
 
 state = {
+    display:'',
     input:'',
     operation: '',
     firstVal: 0,
-    secondValue:0,
     total: ''
 }
 
 handleAdd(numVal){
     this.setState({input: this.state.input + numVal.toString()})
+    this.setState({display:this.state.display.concat(numVal)})
 }
 handleClear(e){
-    this.setState({input: '', operation:'', firstVal:0,})
+    this.setState({input: '', operation:'', firstVal:0, display:''})
 }
 
 handleOppclick(op){
     this.setState({operation: op, firstVal: this.state.input, input: ''})
+    this.setState({display:this.state.display.concat(` ${op} `)})
 }
 
  handleEqualClick(e){
-    
     const secondValue = this.state.input;
     let answer = this.state.firstVal.concat(this.state.operation).concat(secondValue)
     this.setState({input: eval(answer).toString()});
-    
+    this.setState({display: eval(answer).toString()}) 
 }
 
 
 render(){
     return( 
         <div>
-            <Input total = {this.state.input}/>
+            <Display text = {this.state.display}/>
             <NumButton onClick ={numVal => this.handleAdd(numVal)}numVal = {0}/>
             <NumButton onClick ={numVal => this.handleAdd(numVal)}numVal = {1}/>
             <NumButton onClick ={numVal => this.handleAdd(numVal)}numVal = {2}/>
